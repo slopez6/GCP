d98182b32569e1a217a4137b767f0a6c435f6a3c
Sam <105896750+slopez6@users.noreply.github.com>
2025-01-29 15:33:14 -0600

    cloud_run_function-cloud_sql_postgres.py

--- Cloud SQL/Cloud_run_function to Cloud_SQL_Postgres/cloud_run_function-cloud_sql_postgres.py
+++ Cloud SQL/Cloud_run_function to Cloud_SQL_Postgres/cloud_run_function-cloud_sql_postgres.py
@@ -1,53 +1,78 @@
+import functions_framework
 import os
+
 from google.cloud.sql.connector import Connector, IPTypes
 import pg8000
+
 import sqlalchemy
-import google.auth
 
-# credentials, project = google.auth.default()
 
-# initialize Connector object
-connector = Connector()
+def connect_with_connector() -> sqlalchemy.engine.base.Engine:
+    """
+    Initializes a connection pool for a Cloud SQL instance of Postgres.
 
-INSTANCE_CONNECTION_NAME = "lateral-plane-409204:us-central1:test-db"
-DB_USER = "postgres"
-DB_PASS = "123qweasdzxc"
-DB_NAME = "postgres"
+    Uses the Cloud SQL Python Connector package.
+    """
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
 
-# function to return the database connection object
-def getconn():
-    conn = connector.connect(
-        INSTANCE_CONNECTION_NAME,
-        "pg8000",
-        user=DB_USER,
-        password=DB_PASS,
-        db=DB_NAME
-    )
-    print("Connection complete ... ")
-    return conn
+    instance_connection_name = "lopezsamuel-pf-dev:us-central1:test-cloudrun" # e.g. 'project:region:instance'
+    db_user = "sam"  # e.g. 'my-db-user'
+    db_pass = "123456789"  # e.g. 'my-db-password'
+    db_name = "test-db"  # e.g. 'my-database'
 
-def main(request):
-    print("Preparing pool ... ")
-    # create connection pool with 'creator' argument to our connection object function
+    ip_type = IPTypes.PRIVATE if os.environ.get("PRIVATE_IP") else IPTypes.PUBLIC
+
+    # initialize Cloud SQL Python Connector object
+    connector = Connector()
+
+    def getconn() -> pg8000.dbapi.Connection:
+        conn: pg8000.dbapi.Connection = connector.connect(
+            instance_connection_name,
+            "pg8000",
+            user=db_user,
+            password=db_pass,
+            db=db_name,
+            ip_type=ip_type,
+        )
+        return conn
+
+    # The Cloud SQL Python Connector can be used with SQLAlchemy
+    # using the 'creator' argument to 'create_engine'
     pool = sqlalchemy.create_engine(
         "postgresql+pg8000://",
         creator=getconn,
+        # ...
     )
+    return pool
+
+@functions_framework.http
+def main(request):
+    print("Preparing pool ... ")
+    # create connection pool with 'creator' argument to our connection object function
+    pool = connect_with_connector()
+    
 
     with pool.connect() as db_conn:
+        # Insert specific rows into the table
+        ids_to_insert = [2030, 2040, 2050]
+        for id_value in ids_to_insert:
+            db_conn.execute(sqlalchemy.text("INSERT INTO andreas (ID) VALUES (:id)"), {"id": id_value})
+        
+        print("Inserted rows: ", ids_to_insert)
 
         # query database
-        result = db_conn.execute(sqlalchemy.text("SELECT * from home_user")).fetchall()
+        result = db_conn.execute(sqlalchemy.text("SELECT * from andreas")).fetchall()
         print("Ran query ... ")
+        
         # commit transaction (SQLAlchemy v2.X.X is commit as you go)
         db_conn.commit()
 
         # Do something with the results
         for row in result:
             print(row)
-        connector.close()
-    
+
     return 'Ok'
 
-if __name__ == '__main__':
-    main('')
